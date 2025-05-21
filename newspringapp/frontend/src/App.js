876d03ee7d8723dc00ebb7150772c2c3c49854b1
Nandhitha-S19 <nandhitha.s1922@gmail.com>
2025-05-21 23:47:44 +0530

    updated newspringapp folder

---
+++ newspringapp/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Alumni Connect Portal</h1>
+      <p>This is the starting point of your React frontend.</p>
+    </div>
+  );
+}
+
+export default App;
+
